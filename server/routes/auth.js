30218311213ea5b93d4d406f9e6011c1a9b0b049
Abhay Gupta <adnorindia@gmail.com>
2022-02-17 10:22:00 +0530

    adding data validation(express-validator)

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const User = require('../models/User');
+const {body, validationResult} = require('express-validator');
 const router = express.Router();
 
 
 
 
 // Create a user using: POST "/api/auth". Does not require auth
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body); 
+router.post('/',
+    body('email').isEmail(),
+    body('password').isLength({min:5}),
+    (req, res)=>{
+        // Finds the validation wrrors in this request and wraps them in an object with handu functions
+    const errors = validationResult(req);    
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    } 
+    // console.log(req.body);
+    // const user = User(req.body);
+    // user.save(); 
+    // res.send(req.body); 
+    User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: req.body.password,
+        email:req.body.email,
+      }).then(user => res.json(user))
+        .catch(error=>{console.log(error)
+        res.json({erroe: 'Please Enter unique value'})});
 })
 
 module.exports = router
