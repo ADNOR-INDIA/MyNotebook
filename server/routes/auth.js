322d76d763ba3b194c03d8aef98eaa49c99df00e
Abhay Gupta <adnorindia@gmail.com>
2022-02-20 12:19:00 +0530

    hashing passwords using bcryptjs

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,32 +2,52 @@ const express = require('express');
 const User = require('../models/User');
 const {body, validationResult} = require('express-validator');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 
 
 
 // Create a user using: POST "/api/auth". Does not require auth
-router.post('/',
-    body('email').isEmail(),
-    body('password').isLength({min:5}),
-    (req, res)=>{
-        // Finds the validation wrrors in this request and wraps them in an object with handu functions
+router.post('/createUser',
+    [body('email').isEmail(),
+    body('password').isLength({min:5})],
+    async(req, res)=>{
+        // Finds the validation errors in this request and wraps them in an object with handy functions
     const errors = validationResult(req);    
     if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()});
+        return res.status(400).json({errors:errors.array()}); 
     } 
     // console.log(req.body);
     // const user = User(req.body);
     // user.save(); 
     // res.send(req.body); 
-    User.create({
+
+    // check whether the user with this email exists already 
+    try{
+    let user =await User.findOne({email: req.body.email});
+    if (user){
+        return res.status(400).json({error: "sorry user with this email already exists"}) 
+    }
+    // making a secures password using bcrypt
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
+    // creating new user.
+    user= await User.create({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
-        password: req.body.password,
+        password: secPass,
         email:req.body.email,
-      }).then(user => res.json(user))
-        .catch(error=>{console.log(error)
-        res.json({erroe: 'Please Enter unique value'})});
+      })
+      res.json(user)
+    }
+    catch (error){
+        console.log(error.message);
+        res.status(500).send("some error occured");
+    }
+      
+        // .then(user => res.json(user))
+        // .catch(error=>{console.log(error)
+        // res.json({error: 'Please Enter unique value'})});
 })
 
 module.exports = router
