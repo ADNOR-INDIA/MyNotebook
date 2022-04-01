e6abcc8eaf2ae786650ae094061d6bf9fb3ef90d
Abhay Gupta <adnorindia@gmail.com>
2022-04-01 12:38:24 +0530

    adding notes from server

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -1,15 +1,9 @@
 import React,{ useContext, useEffect } from "react";
-import NoteContext from "../context/notes/noteContext";
 
 const About=()=>{
-    const a = useContext(NoteContext)
-    useEffect(()=>{ 
-        a.update();
-        // eslint-dsiable-next-lines
-    }, [])
     return(
         <div>
-            this is about {a.state.name}.
+            this is about .
         </div>
     )
 }
--- client/src/context/notes/NoteState.js
+++ client/src/context/notes/NoteState.js
@@ -1,21 +1,20 @@
 import NoteContext from "./noteContext";
 import { useState } from "react";
 const NoteState=(props)=>{
-    const s1={
-        "name":"Abhay",
-        "class":"5b "
-    }
-    const [state, setState] = useState(s1);
-    const update=()=>{
-        setTimeout(()=>{
-            setState({
-                "name":"chobey",
-                "class":"4th sem" 
-            })
-        },1000);
-    }
+    const notesInitial = [
+        {
+          "_id": "62146843ec30302647b747ef",
+          "user": "6211d02a162015b3e6ca8111",
+          "title": "metitle",
+          "description": "do coding af",
+          "tag": "personell",
+          "date": "2022-02-22T04:36:19.705Z",
+          "__v": 0
+        }
+      ]
+      const[notes, setNotes] = useState(notesInitial);
     return (
-        <NoteContext.Provider value = {{state, update}}>
+        <NoteContext.Provider value = {{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
